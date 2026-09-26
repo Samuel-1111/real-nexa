8a2f2c0dbfdac3bae3e29fe4fdc69aa92345f2cc
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-26 05:33:45 -0700

    Bump NEXA service worker cache for install updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="nexa-shell-v6";
+const CACHE_NAME="nexa-shell-v7";
 const APP_SHELL=["/","/auth","/auth/reset","/manifest.webmanifest","/icon-192.svg","/icon-512.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
